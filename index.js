2646d1633906a1d49db1c54d6016cc02cfd076b4
Amanda <amandaspolaor.aluno@unipampa.edu.br>
2021-08-26 15:28:42 -0300

    login

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ M.textareaAutoResize($('#message'));
 $(document).ready($(".dropdown-trigger").dropdown());
 
 
+$(document).ready(function () {
+    M.updateTextFields();
+});
+
+const list = document.querySelectorAll('.list');
+function activeLink() {
+    list.forEach((item) =>
+        item.classList.remove('active'));
+    this.classList.add('active');
+}
+list.forEach((item) =>
+    item.addEventListener('click', activeLink));
